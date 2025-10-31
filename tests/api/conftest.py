d12f91694680b263fef0812701d240bb330da5a1
Medallion <nickcar13@gmail.com>
2025-10-30 23:17:38 -0500

    feat: adding red test for PUT /users/{user_id}

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -1,10 +1,19 @@
 """ Configuracion para test del API"""
 
 import pytest
+from fastapi.testclient import TestClient
 
-from app.models import UserCreate
+from app.models import UserCreate, UserResponse
 
 
 @pytest.fixture
 def sample_user() -> UserCreate:
     return UserCreate(username="Test User", email="test@test.com", password="Test password")
+
+
+@pytest.fixture
+def created_user(client: TestClient, sample_user: UserCreate) -> UserResponse:
+    create_response = client.post("/users", json=sample_user.model_dump())
+    assert create_response.status_code == 201
+    data = create_response.json()
+    return UserResponse.model_validate(data)
--- tests/api/test_users.py
+++ tests/api/test_users.py
@@ -1,8 +1,9 @@
 from fastapi.testclient import TestClient
 
-from app.models import UserCreate
+from app.models import UserCreate, UserResponse, UserUpdate
 
 
+# Testing POST /users/{user_id} endpoint
 def test_create_user(client: TestClient, sample_user: UserCreate) -> None:
     response = client.post("/users", json=sample_user.model_dump())
 
@@ -26,22 +27,56 @@ def test_create_user_missing_password(client: TestClient) -> None:
     assert data["detail"][0]["loc"] == ["body", "password"]
 
 
-def test_get_user_by_id(client: TestClient, sample_user: UserCreate) -> None:
-    create_response = client.post("/users", json=sample_user.model_dump())
-    assert create_response.status_code == 201
-    user_id = create_response.json()["id"]
+# Testing POST /users/{user_id} endpoint
+def test_get_user_by_id(client: TestClient, created_user: UserResponse) -> None:
+    user_id = created_user.id
 
     response = client.get(f"/users/{user_id}")
 
     assert response.status_code == 200
     data = response.json()
     assert data["id"] == user_id
-    assert data["username"] == sample_user.username
+    assert data["username"] == created_user.username
     assert "password" not in data
 
 
 def test_get_user_not_found(client: TestClient) -> None:
     response = client.get("/users/99999")
+
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found"}
+
+
+# Testing PUT /users/{user_id}
+def test_update_user(client: TestClient, created_user: UserResponse) -> None:
+    user_id = created_user.id
+
+    update_payload = UserUpdate(username="new_username", email="new_email")
+    response = client.put(f"/users/{user_id}", json=update_payload.model_dump(exclude_unset=True))
+
+    assert response.status_code == 200
+    data = response.json()
+    assert data["id"] == user_id
+    assert data["username"] == update_payload.username
+    assert data["email"] == update_payload.email
+
+
+def test_partial_update_user(client: TestClient, created_user: UserResponse) -> None:
+    user_id = created_user.id
+
+    update_payload = UserUpdate(username="new_username")
+    response = client.put(f"/users/{user_id}", json=update_payload.model_dump(exclude_unset=True))
+
+    assert response.status_code == 200
+    data = response.json()
+    assert data["id"] == user_id
+    assert data["username"] == update_payload.username
+    assert data["email"] == created_user.email
+
+
+def test_update_user_not_found(client: TestClient) -> None:
+    update_payload = UserUpdate(username="user_name")
+    response = client.put("/users/99999", json=update_payload.model_dump(exclude_unset=True))
 
     assert response.status_code == 404
     assert response.json() == {"detail": "User not found"}

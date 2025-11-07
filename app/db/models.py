ca9ff45baa1a0d3142dbbf4f4a84c8986b7ee024
Medallion <nickcar13@gmail.com>
2025-11-06 23:01:22 -0500

    feat(db): Create inital sqlalchemy models and refactor schemas

---
+++ app/db/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    """Metadata class"""
+
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
